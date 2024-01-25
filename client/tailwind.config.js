cdcb06d89652545f037baa8c5ae7440f9eaba641
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2024-01-25 14:27:11 +0100

    add custom screen breakpoint and custom border width

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        "custom-xl": "0 0 24px 4px black",
+      // boxShadow: {
+      //   "custom-xl": "0 0 24px 4px black",
+      // },
+      screens: {
+        "3xl": "2000px",
+      },
+      borderWidth: {
+        1: "0.5px",
       },
     },
   },
