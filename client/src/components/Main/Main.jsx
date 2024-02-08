b6937a4118b53d255c7e151841a83269fa13b07c
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2024-02-08 15:48:25 +0100

    added routes to display different components, depending on subpage

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Post from "./Post/Post";
+import ProfileContent from "./ProfileContent/ProfileContent";
 import "./Main.scss";
 
 const Main = () => {
   return (
     <main className="main">
       <section className="main__posts">
-        <Post />
+        <Routes>
+          <Route
+            path="/"
+            element={<Post isPostingActive={true} isCommentingActive={false} />}
+          />
+          <Route path="/exampleProfile" element={<ProfileContent />} />
+          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        </Routes>
       </section>
     </main>
   );
