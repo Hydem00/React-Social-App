ada3c57450d3e1508f0e015cb2f245eaa4f70c07
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2024-02-08 15:46:02 +0100

    added functionality to display modal and transfer this to specific components by props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Aside from "./components/Aside/Aside";
 import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import ModalAddPost from "./components/Main/Post/ModalAddPost/ModalAddPost";
 
 function App() {
+  const [isModalAddPostOpen, setIsModalAddPostOpen] = useState(false);
+
+  const handleAddPostModal = () => {
+    setIsModalAddPostOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalAddPostOpen(false);
+  };
+
   return (
     <div className="App">
       <div className="wrapper">
-        <Header />
-        <Main />
-        <Aside />
+        <Router>
+          <Header addPostModal={handleAddPostModal} />
+          <Main />
+          <Aside />
+          <ModalAddPost
+            isOpen={isModalAddPostOpen}
+            onClose={handleCloseModal}
+          />
+        </Router>
       </div>
     </div>
   );
