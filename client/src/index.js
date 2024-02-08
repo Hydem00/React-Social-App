ed2473179e39fd431bfffe35c45eb0c7a38d6304
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2024-02-08 19:03:23 +0100

    added StoreProvider at the top of the component tree

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import StoreProvider from "./components/store/StoreProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
