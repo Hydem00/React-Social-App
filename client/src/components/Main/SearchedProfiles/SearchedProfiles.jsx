6b724ab6239ebc51b08c9aeb7384bff75e143f82
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2024-02-13 11:03:56 +0100

    added functionality to display searched profiles

--- client/src/components/Main/SearchedProfiles/SearchedProfiles.jsx
+++ client/src/components/Main/SearchedProfiles/SearchedProfiles.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import Profiles from "../../Aside/Profiles/Profiles";
 import "./SearchedProfiles.scss";
 
 const SearchedProfiles = () => {
+  const [searchedProfiles, setSearchedProfiles] = useState({});
+  const { name } = useParams();
+
+  const fetchSearchedProfiles = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/api/users/${name}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+
+      const result = response.data;
+      // console.log(result);
+      if (!result.data.isMe) setSearchedProfiles(result.data);
+      else setSearchedProfiles({});
+    } catch (error) {
+      console.error("Wystąpił błąd przy pobieraniu danych:", error);
+      setSearchedProfiles({});
+    }
+  };
+
+  useEffect(() => {
+    fetchSearchedProfiles();
+  }, [name]);
+
   return (
     <div className="searched-profiles">
       <h2 className="searched-profiles__headline">Searched Profiles</h2>
-      <Profiles />
+      {searchedProfiles && Object.keys(searchedProfiles).length !== 0 ? (
+        <Profiles
+          isSearchedProfileActive={true}
+          searchedProfiles={searchedProfiles}
+          fetchSearchedProfiles={fetchSearchedProfiles}
+        />
+      ) : (
+        <div className="text-center">There is no such a profile!</div>
+      )}
     </div>
   );
 };
